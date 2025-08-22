1b70651c657181feaf5bd8760ea163dbccacf3f0
myarava01 <myarava01@gmail.com>
2025-08-22 09:12:32 +0530

    `Added newline at end of file in setup.py`

--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
-from os import name
 from setuptools import setup, find_packages
 
-set (
+setup(
     name='my_package',
     version='1.0',
     packages=find_packages(),
     install_requires=['numpy', 'pandas'],
     author='Your Name',
     author_email='your_email@example.com',
-    )               
-
-set (
-    name:'memoryview'
-    version:'1.0'
-    packages:find_packages()
-    install_requires: ['numpy', 'pandas']
-    author:'Your Name'
-    author_email:'your_email@example.com'
 )
-
-set (
-    name:'Shart kotla'
-    version:'1.0'
-    packages:find_packages()
-    install_requires: ['numpy', 'pandas']
-    author:'Your Name'
-    author_email:'your_email@example.com'
-)
-
-set (
-    name:'Shart kotla'
-    version:'1.0'
-    packages:find_packages()
-    install_requires: ['numpy', 'pandas']
-    author:'Your Name'
-    author_email:'your_email@example.com'
-)
-
-set 
