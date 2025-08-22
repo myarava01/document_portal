62637bcbbbbfb4af269e2de6156eb4b384f39e53
myarava01 <myarava01@gmail.com>
2025-08-22 08:52:16 +0530

    few changes

---
+++ setup.py
@@ -0,0 +1,40 @@
+from os import name
+from setuptools import setup, find_packages
+
+set (
+    name='my_package',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas'],
+    author='Your Name',
+    author_email='your_email@example.com',
+    )               
+
+set (
+    name:'memoryview'
+    version:'1.0'
+    packages:find_packages()
+    install_requires: ['numpy', 'pandas']
+    author:'Your Name'
+    author_email:'your_email@example.com'
+)
+
+set (
+    name:'Shart kotla'
+    version:'1.0'
+    packages:find_packages()
+    install_requires: ['numpy', 'pandas']
+    author:'Your Name'
+    author_email:'your_email@example.com'
+)
+
+set (
+    name:'Shart kotla'
+    version:'1.0'
+    packages:find_packages()
+    install_requires: ['numpy', 'pandas']
+    author:'Your Name'
+    author_email:'your_email@example.com'
+)
+
+set 
